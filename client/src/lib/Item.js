76ff95b758d9e3b55c322e62c53a44f2a8617acb
yashweblife <yashweblife20@gmail.com>
2024-11-21 14:12:03 -0600

    make the code server compatible

--- client/src/lib/Item.js
+++ client/src/lib/Item.js
@@ -1,31 +1,96 @@
-import { DOM, generateId } from "./index.js";
+import { DOM } from "./index.js";
 export class Item {
-    dom=null;
+    dom = null;
     completed = false
-    id=""
-    date=""
-    constructor(title){
-        this.createDOM(title)
-        this.id = generateId()
-        this.date = Date.now()
+    id = ""
+    date = ""
+    title = ""
+    constructor(title, completed=false) {
+        this.title = title
+        this.completed = completed
+        this.createDOM()
     }
-    createDOM(title){
+    createDOM() {
         const item = new DOM("div").addClass("item")
-        const header = new DOM("div").addClass("header").text(title)
+        if(this.completed===true) item.addClass("complete")
+        const header = new DOM("div").addClass("header").text(this.title)
         const footer = new DOM("div").addClass("footer")
         const removeButton = new DOM("button").text("Remove").addEvent("click", () => this.remove())
-        const completeButton = new DOM("button").text("Complete").addEvent("click", () => this.complete()) 
+        const completeButton = new DOM("button").text("Complete").addEvent("click", () => this.complete())
         footer.append(removeButton).append(completeButton)
         item.append(header).append(footer)
         this.dom = item
         return this.dom
     }
-    remove(){
+    remove() {
         this.dom.remove()
         this.dom = null;
+        this.removeFromDB()
     }
-    complete(){
+    complete() {
         this.dom.toggleClass("complete")
         this.completed = !this.completed
+        this.updateDB()
+    }
+    async uploadToDB() {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/todos`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    title: this.title,
+                    completed: this.completed,
+                })
+            })
+            const {id} = await response.json()
+            this.id = id
+            return data
+        } catch (error) {
+            console.log(error)
+            return error
+        }
+    }
+    async updateDB() {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/todos`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    todo: {
+                        title: this.title,
+                        completed: this.completed,
+                        date: this.date
+                    },
+                    id: this.id
+                })
+            })
+            const data = await response.json()
+            return data
+        } catch (error) {
+            console.log(error)
+            return error
+        }
+    }
+    async removeFromDB() {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/todos/`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    id: this.id
+                })
+            })
+            const data = await response.json()
+            return data
+        } catch (error) {
+            console.log(error)
+            return error
+        }
     }
 }
--- client/src/lib/List.js
+++ client/src/lib/List.js
@@ -10,7 +10,28 @@ export class List {
         this.submit = DOM.fromDom(submitName)
         this.submit.addEvent("click", ()=>{
             const item = new Item(this.input.getValue())
+            item.uploadToDB()
+            this.input.dom.value = ""
             this.list.append(item.dom)
         })
+        this.getInitialTodos()
+    }
+    async getInitialTodos(){
+        try {
+            const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/todos`, {
+                method: "GET"
+            })
+            const data = await response.json()
+            data.todos.forEach((todo)=>{
+                if(!todo) return
+                const item = new Item(todo.title||"", todo.completed)
+                item.date = todo.date
+                item.id = todo.id
+                this.list.append(item.dom)
+            })
+        } catch(error) {
+            console.log(error)
+            return error
+        }
     }
 }
