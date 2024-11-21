2373b32ea5577c33e04c4e6657d427f97de8f71c
yashweblife <yashweblife20@gmail.com>
2024-11-21 12:04:46 -0600

    more config

--- server/jest.config.js
+++ server/jest.config.js
@@ -4,7 +4,7 @@
  */
 
 /** @type {import('jest').Config} */
-const config = {
+export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -194,5 +194,3 @@ const config = {
   // Whether to use watchman for file crawling
   // watchman: true,
 };
-
-module.exports = config;
--- server/src/tests/index.test.js
+++ server/src/tests/index.test.js
@@ -1,3 +1,18 @@
-test('test',()=>{
-    expect(1).toBe(1)
-})
+import assert from 'node:assert'
+import test from 'node:test'
+test('DATABASE TESTS', async (t) => {
+
+    const res = await fetch('http://localhost:3030/api/todos', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'hello',
+            completed: false
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const {success }= await res.json()
+    assert.strictEqual( success, true)
+}
+)
